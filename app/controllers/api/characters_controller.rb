61cc512c6e14e58e1cf580237556e967aae11612
natsumi <natsu.bsk810@gmail.com>
2024-07-16 02:12:04 +0900

    add

---
+++ app/controllers/api/characters_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  class CharactersController < ApplicationController
+    def index
+      @characters = Character.all
+      render json: @characters
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # resources :lists
   resources :characters
 
+  namespace :api do
+    resources :characters, only: [:index]
+  end
 end
